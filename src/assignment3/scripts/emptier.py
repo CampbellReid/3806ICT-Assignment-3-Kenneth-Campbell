3ed54b28600159e3b2c2fb7d600d8d75db53fc3b
Campbell Reid <campbell@campbellreid.com>
2024-05-20 12:26:48 +1000

    Added stubs for collector, explorer, and emptier

---
+++ src/assignment3/scripts/emptier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+
+def emptier():
+    rospy.init_node('emptier')
+    
+    # Add your code here
+    
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        emptier()
+    except rospy.ROSInterruptException:
+        pass
