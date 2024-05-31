2921a2d1d54e51ed604ef89b7d058c3f0fadff21
Campbell Reid <campbell@campbellreid.com>
2024-05-31 19:45:36 +1000

    Fix activate script

--- ros-post-activate.sh
+++ ros-post-activate.sh
@@ -3,16 +3,14 @@
 # Source ROS Noetic setup
 source /opt/ros/noetic/setup.bash
 
-export ASSIGNMENT_FOLDER_PARENT="/home/campbell/repos"
-
 # Source the workspace setup
-source "${ASSIGNMENT_FOLDER_PARENT}/3806ICT-Assignment-3-Kenneth-Campbell/devel/setup.bash"
+source /home/campbell/repos/3806ICT-Assignment-3-Kenneth-Campbell/devel/setup.bash
 
 # Set TURTLEBOT3 model
 export TURTLEBOT3_MODEL=burger
 
 # Update PYTHONPATH to include local site-packages and conda environment
-export PYTHONPATH="${HOME}/miniconda3/envs/ros_noetic/lib/python3.8/site-packages:$PYTHONPATH"
+export PYTHONPATH=/home/campbell/repos/3806ICT-Assignment-3-Kenneth-Campbell/src/assignment3/scripts:/home/campbell/miniconda3/envs/ros_noetic/lib/python3.8/site-packages:$PYTHONPATH
 
 # Update PATH to include conda environment
-export PATH="${HOME}/miniconda3/envs/ros_noetic/bin:$PATH"
+export PATH=/home/campbell/miniconda3/envs/ros_noetic/bin:$PATH(ros_noetic)
